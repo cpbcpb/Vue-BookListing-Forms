0ad6070774c47ecc91e7e72ede7492cb7408d1e7
Christine Beck <39750121+cpbcpb@users.noreply.github.com>
2019-12-17 14:42:16 -0500

    Added a search

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+<div>
     <h1>{{title}}</h1>
-
+    <input type="text" v-model="searchInput" placeholder="Search Books">
     <ul>
-      <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+        <book-item v-for='book in searchedBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <hr>
     <h2>Filtered Books By Ownership</h2>
@@ -13,12 +13,13 @@
         </option>
     </select>
     <ul>
-          <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+        <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
     </ul>
 
-    <br><hr>
+    <br>
+    <hr>
     <book-form @addBook='appendBook'></book-form>
-  </div>
+</div>
 </template>
 
 <script>
@@ -27,56 +28,76 @@ import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
 export default {
-  name: "BookList",
-  components: {
-    BookItem,
-    BookForm
-  },
-  data() {
-    return {
-      title: "All Books",
-      states: ["Want to Read", "Read", "Reading"],
-      filters:["bought", "borrowed"],
-      holding: "bought",
-      books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, 
-        ownership:"borrowed" },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false,
-        ownership:"bought" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading:true,
-        ownership:"borrowed" }
-      ]
-    };
-  },
-  computed: {
-    filteredBooks(){
-      return _.filter(this.books, ["ownership", this.holding])
+    name: "BookList",
+    components: {
+        BookItem,
+        BookForm
+    },
+    data() {
+        return {
+            title: "All Books",
+            states: ["Want to Read", "Read", "Reading"],
+            filters: ["bought", "borrowed"],
+            holding: "bought",
+            searchInput: "",
+            books: [{
+                    title: "Self-Reliance",
+                    author: "Ralph Waldo Emerson",
+                    finishedReading: true,
+                    ownership: "borrowed"
+                },
+                {
+                    title: "American Gods",
+                    author: "Neil Gaiman",
+                    finishedReading: false,
+                    ownership: "bought"
+                },
+                {
+                    title: "Amusing Ourselves to Death",
+                    author: "Neil Postman",
+                    finishedReading: true,
+                    ownership: "borrowed"
+                }
+            ]
+        };
+    },
+    computed: {
+        filteredBooks() {
+            return _.filter(this.books, ["ownership", this.holding])
+        },
+        searchedBooks() {
+            const searchFilter = book => {
+                return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+            }
+            return _.filter(this.books, searchFilter);
+        }
+    },
+    methods: {
+        appendBook(bookData) {
+            this.books.push({
+                title: bookData.bookTitle,
+                author: bookData.bookAuthor,
+                finishedReading: bookData.finishedReading,
+                ownership: bookData.ownership
+            });
+        }
     }
-  },
-  methods: {
-    appendBook(bookData) {
-      this.books.push({ 
-          title: bookData.bookTitle, 
-          author: bookData.bookAuthor, 
-          finishedReading:bookData.finishedReading,
-          ownership:bookData.ownership});
-    }
-  }
 };
 </script>
 
 <style>
 h1,
 h2 {
-  font-weight: normal;
+    font-weight: normal;
 }
+
 ul {
-  list-style-type: none;
-  padding: 0;
+    list-style-type: none;
+    padding: 0;
 }
 
 li {
-  display: block;
-  margin: 0 10px;
+    display: block;
+    margin: 0 10px;
 }
 </style>
